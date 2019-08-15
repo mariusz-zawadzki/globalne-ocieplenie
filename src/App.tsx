bb0d6818a9e11e97f8cb5b19d436f43afb9e3f23
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2019-08-15 23:13:36 +0200

    added better select with combobox option minor refactor with some option removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
-// Add the Firebase products that you want to use
-import "firebase/firestore";
-import {Chart, Cities, DAYS} from "./chart/Chart";
+import {Chart } from "./chart/Chart";
 
 
 const App: React.FC = () => {
-    const updateCity = (city: string)=> {
-        console.log("new city", city)
-    };
-    const updateYear = (city: string)=> {
-        console.log("new myear", city)
-    };
     return (
     <div className="App">
         <Chart />
